7b4bdb55394e383fe42e065c82b5df82cdaa7f4e
nadya.chervyakova <kraynova06@yandex.ru>
2021-11-12 12:15:58 +0300

    25

--- SQL/tasks_sql-ex.sql
+++ SQL/tasks_sql-ex.sql
@@ -323,3 +323,23 @@ maker
 A
 B
 ----------------------------------------------------------------------
+Задание: 25
+Найдите производителей принтеров, которые производят ПК с наименьшим объемом RAM и с самым быстрым процессором среди всех ПК, имеющих наименьший объем RAM. Вывести: Maker
+
+SELECT DISTINCT maker 
+FROM Product 
+WHERE model IN (SELECT model FROM PC
+  WHERE ram IN (SELECT MIN(ram) FROM pc)
+  AND 
+  speed IN (SELECT MAX(speed) FROM PC
+  WHERE ram IN (SELECT MIN(ram) FROM PC))
+)
+AND
+maker IN (SELECT maker FROM Product
+  WHERE type = 'Printer');
+
+maker
+A
+E
+----------------------------------------------------------------------
+
